28da9c9b4a4804368066d4512af65bc617de19bb
Shema Leandre <iamshemaleandre@gmail.com>
2025-10-27 21:22:31 +0200

    Add signed APK example

---
+++ examples/signed-apk.sh
@@ -0,0 +1,12 @@
+﻿#!/usr/bin/env bash
+# Example: Generate signed APK
+
+../builder.sh \
+  --keystore ../release.keystore \
+  --keystore-pass "my_keystore_password" \
+  --alias "my_key_alias" \
+  --key-pass "my_key_password" \
+  --universal \
+  --optimize \
+  --output ./signed-output \
+  myapp.aab
