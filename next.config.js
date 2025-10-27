697bb2f4fb7cfe3d773ed77dacfaefe2713e0285
Shema Leandre <iamshemaleandre@gmail.com>
2025-10-27 21:34:57 +0200

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,18 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['localhost'],
+  },
+  experimental: {
+    serverActions: true,
+  },
+  api: {
+    bodyParser: {
+      sizeLimit: '100mb',
+    },
+  },
+}
+
+module.exports = nextConfig
