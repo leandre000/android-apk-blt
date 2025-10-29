b4c31965aab56fe23e97fb71cf7cdc2a5c1848c3
Shema Leandre <iamshemaleandre@gmail.com>
2025-10-29 09:11:15 +0200

    fixed dev tools

--- aab2apk-web/next.config.ts
+++ aab2apk-web/next.config.ts
@@ -3,6 +3,9 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: path.join(__dirname),
+  experimental: {
+    turbo: undefined, // Disable Turbopack
+  },
 };
 
 export default nextConfig;
